56feeb92536e63796fdc09355d960782faf559be
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-11-14 13:36:04 -0800

    bootstrapped a react application and fetched categories data from the backend api

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import * as API from './utils/api';
+import './App.css';
+
+class App extends Component {
+  state = {
+    categories: []
+  }
+  componentDidMount() {
+    API.getAll().then((categories) => {
+      this.setState({
+        categories: categories
+      })
+      console.log(this.state.categories)
+    })
+  }
+  render() {
+    return (
+      <div>
+      Hello World!!
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,11 @@
+const url = 'http://localhost:3001'
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': 'auth'
+}
+
+export const getAll = () =>
+  fetch(`${url}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
