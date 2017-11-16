f32704f8ea76ace84ab3b2594590d30e00655c7b
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-11-16 14:34:45 -0800

    imported Provider from react-redux npm package

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
-console.log(store);
+console.log(store.getState());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+  <App />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
