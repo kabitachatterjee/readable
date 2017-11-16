c559b2d36da31b554f5093378ac118fd9e7f35a8
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-11-16 14:27:37 -0800

    created a store with the reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
-import * as Api from '../utils/Api';
+import * as Api from '../utils/api';
+import { receive } from '../utils/helper';
 
-export constant FETCH_POSTS as 'FETCH_POSTS';
+export const FETCH_POSTS = 'FETCH_POSTS';
 
 export const fetchPosts = () => dispatch =>
   Api.getPosts().then(payload => dispatch(receive(FETCH_POSTS, payload)));
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+const store = createStore(reducer);
+console.log(store);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
