e94fc5fda4d2e962c1bdf50d382dfba6dae7236a
kabitachatterjee <chatterjeekavita01@gmail.com>
2017-11-29 15:19:32 -0800

    got the post to show up on a post page

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import { getPost } from '../actions';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class PostShow extends Component {
+  static propTypes = {
+    post: PropTypes.object,
+    getPost: PropTypes.func.isRequired
+  };
+  componentDidMount() {
+    const { postId } = this.props.match.params;
+    console.log(postId);
+
+    this.props.getPost(postId);
+
+  }
 
   render() {
+    const { post } = this.props;
+    console.log(post);
     return(
       <div>
-      Showing a post
+      <h3>{post.title}</h3>
+      <p>{post.body}</p>
+      <em> -{post.author}</em> at {post.timestamp}
       </div>
     )
   }
 
 }
 
-export default PostShow;
+function mapStateToProps({ posts }, { match }) {
+  return {
+    post: posts.filter(post => post.id === match.params.postId)[0]
+  };
+}
+
+export default connect(mapStateToProps, {
+  getPost
+})(PostShow);
+
+//export default PostShow;
